152a0136b7ca0770d3f194d4e052946316ed3ae8
ramonroman <ramonroman@users.noreply.github.com>
2022-08-16 20:37:53 -0300

    Primeiro Commit

---
+++ mesa10Ago/calc.js
@@ -0,0 +1,33 @@
+function add(n1, n2) {
+    let result = n1 + n2;
+    return result;
+}
+console.log(`Teste de operação matemática do tipo: Soma ${add(1, 1)}`);
+//console.log(add(1, 1));
+
+function subtract(n1, n2) {
+    let result = n1 - n2;
+    return result;
+}
+console.log(`Teste de operação matemática do tipo: Subtração ${subtract(1, 1)}`);
+//console.log(subtract(1, 1));
+
+function multiply(n1, n2) {
+    let result = n1 * n2;
+    return result;
+}
+console.log(`Teste de operação matemática do tipo: Multiplicação ${multiply(5, 5)}`);
+//console.log(multiply(5, 5));
+
+function dividedBy(n1, n2) {
+    let result = n1 / n2;
+    return result;
+}
+console.log(`Teste de operação matemática do tipo: Divisão ${dividedBy(10, 2)}`);
+console.log(`Teste de operação matemática do tipo: Divisão por 0 ${dividedBy(10, 0)}`);
+//console.log(dividedBy(10, 2));
+
+function squareNumber(n) {
+
+}
+console.log(squareNumber(10));
---
+++ mesa15Ago/mesa15Ago.js
@@ -0,0 +1,39 @@
+//1).!true == false;
+
+//2).!false == true;
+
+//3). !!false true == true false;
+
+//4). !!true == true;
+
+//5).!1 == false;
+
+//6).!!1 == true;
+
+//7).!0 == true;
+
+//8).!!0 == false;
+
+//9).!!'' == false;
+
+//10). let x=5;
+//let y=9;
+//a). (x<10 && x!==5) == false;
+//b). (x>9 || x===5) == true;
+//c). !(x===y) == true;
+
+
+//Parte II
+
+//1). let x = 10;
+// let y ='a';
+// (y==='b' ||x >= 10) == true;
+
+//2). let x = 3;
+// let y = 8;
+// (!(x=='3' || x===y)&&!(y!==8 && x <= y)) == false
+
+//3). let str='';
+// let msg='haha';
+// let eBonito ='false';
+//!((str || msg) && eBonito) == false
