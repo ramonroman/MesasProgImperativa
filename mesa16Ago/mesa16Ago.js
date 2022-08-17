2ac95d4e3f5bf0edfdd7ff53f6b19559b6dbe05a
ramonroman <ramonroman@users.noreply.github.com>
2022-08-16 22:41:06 -0300

    corrigindo syntaxe e refatorando código

--- mesa16Ago/mesa16Ago.js
+++ mesa16Ago/mesa16Ago.js
@@ -2,33 +2,31 @@ function podeSubir(height, acompanhada) {
     height,
     acompanhada
 
-    if (height >= 1.40 && height < 2) {
+    if (height > 1.40 && height < 2) {
         return true
-    } else if (height == 1.20 && height <= 1.40 && acompanhada == true) {
+    } else if (height <= 1.40 && height >= 1.20 && acompanhada) {
         return true
     } else {
-        height > 1.20
+        height < 1.20
         return false
     }
 }
-
-console.log(podeSubir(1.9, false))
+console.log(podeSubir(1.20, true))
 
 
 function podeSubirComTexto(height, acompanhada) {
     height,
     acompanhada
 
-    if (height >= 1.40 && height < 2) {
+    if (height > 1.40 && height < 2) {
         console.log('Acesso autorizado');
 
-    } else if (height == 1.20 && height <= 1.40 && acompanhada == true) {
+    } else if (height >= 1.20 && height <= 1.40 && acompanhada) {
         console.log('Acesso autorizado somente com acompanhante');
 
     } else {
-        height > 1.20
+        height < 1.20
         console.log('Acesso negado!');
     }
 }
-
 podeSubirComTexto(1.20, true)
